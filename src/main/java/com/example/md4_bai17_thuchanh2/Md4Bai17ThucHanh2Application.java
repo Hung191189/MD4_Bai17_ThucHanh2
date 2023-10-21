6dc5732b5ad84585bdc030d2ee665584d1183f8d
Hun191189 <tranmanhhung191189@gmai.com>
2023-10-21 13:39:48 +0700

    MD4Bai17TH2

---
+++ src/main/java/com/example/md4_bai17_thuchanh2/Md4Bai17ThucHanh2Application.java
@@ -0,0 +1,13 @@
+package com.example.md4_bai17_thuchanh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4Bai17ThucHanh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4Bai17ThucHanh2Application.class, args);
+    }
+
+}
